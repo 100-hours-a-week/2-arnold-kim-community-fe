c1ad982258798d3c0ce0955761303067fea02b2e
tpgh02 <seaho4588@gmail.com>
2025-03-18 16:46:31 +0900

    [refactor] 프로필사진 클릭시 드롭다운 바 통합

--- component/header.js
+++ component/header.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const userProfile = document.getElementById("user-profile");
         const backToPosts = document.getElementById("back");
+        const profileDropdown = document.createElement("div");
 
         const storedUrl = localStorage.getItem("profileImgUrl");
         if (storedUrl) {
@@ -15,12 +16,46 @@ document.addEventListener("DOMContentLoaded", () => {
             userProfile.src = storedUrl;
         }
         
-        userProfile.addEventListener("click", () => {
-            window.location.href = "../user/editProfile.html";
-        });
-
         backToPosts.addEventListener("click", () => {
             window.location.href = "../board/posts.html";
+        });
+
+        profileDropdown.classList.add("profile-dropdown");
+        profileDropdown.innerHTML = `
+            <ul>
+                <li id="edit-profile-menu">회원정보수정</li>
+                <li id="edit-password-menu">비밀번호수정</li>
+                <li id="logout-menu">로그아웃</li>
+            </ul>
+        `;
+        document.body.appendChild(profileDropdown);
+    
+        function updateDropdownPosition() {
+            const rect = userProfile.getBoundingClientRect();
+            profileDropdown.style.top = `${rect.bottom + window.scrollY + 5}px`; 
+            profileDropdown.style.left = `${rect.left - profileDropdown.offsetWidth - 150}px`; 
+        }
+    
+        userProfile.addEventListener("click", () => {
+            if (profileDropdown.style.display === "block") {
+                profileDropdown.style.display = "none";
+            } else {
+                updateDropdownPosition();
+                profileDropdown.style.display = "block";
+            }
+        });
+    
+        document.getElementById("edit-profile-menu").addEventListener("click", () => {
+            window.location.href = "../user/editProfile.html"
+        })
+    
+        document.getElementById("edit-password-menu").addEventListener("click", () => {
+            window.location.href = "../user/editPassword.html";
+        });
+    
+        document.getElementById("logout-menu").addEventListener("click", () => {
+            window.location.href = "../user/login.html";
+            localStorage.clear();
         });
       })
       .catch((err) => console.error(err));
--- user/js/editProfile.js
+++ user/js/editProfile.js
@@ -1,8 +1,6 @@
 import CONFIG from "../../config.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-    const userProfile = document.getElementById("user-profile");
-    const profileDropdown = document.createElement("div");
     const usernameInput = document.getElementById("username");
     const usernameError = document.getElementById("username-error");
     const userEmail = document.getElementById("email");
@@ -11,9 +9,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     const profileImage = document.getElementById("profile-image");
     const profileUpload = document.getElementById("profile-upload");
     const editCompleteBtn = document.getElementById("edit-profile-complete-btn");
-    const backToPosts = document.getElementById("back");
 
-    // let user;
     let file;
 
     userEmail.textContent = localStorage.getItem("currentUserEmail");
@@ -21,51 +17,8 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     const storedUrl = localStorage.getItem("profileImgUrl");
     if (storedUrl) {
-        userProfile.src = storedUrl;
-        profileImage.src = storedUrl
+        profileImage.src = storedUrl;
     }
-
-    backToPosts.addEventListener("click", () => {
-        window.location.href = "../board/posts.html";
-    });
-
-    profileDropdown.classList.add("profile-dropdown");
-    profileDropdown.innerHTML = `
-        <ul>
-            <li id="edit-profile-menu">회원정보수정</li>
-            <li id="edit-password-menu">비밀번호수정</li>
-            <li id="logout-menu">로그아웃</li>
-        </ul>
-    `;
-    document.body.appendChild(profileDropdown);
-
-    function updateDropdownPosition() {
-        const rect = userProfile.getBoundingClientRect();
-        profileDropdown.style.top = `${rect.bottom + window.scrollY + 5}px`; 
-        profileDropdown.style.left = `${rect.left - profileDropdown.offsetWidth - 150}px`; 
-    }
-
-    userProfile.addEventListener("click", () => {
-        if (profileDropdown.style.display === "block") {
-            profileDropdown.style.display = "none";
-        } else {
-            updateDropdownPosition();
-            profileDropdown.style.display = "block";
-        }
-    });
-
-    document.getElementById("edit-profile-menu").addEventListener("click", () => {
-        window.location.href = "editProfile.html"
-    })
-
-    document.getElementById("edit-password-menu").addEventListener("click", () => {
-        window.location.href = "editPassword.html";
-    });
-
-    document.getElementById("logout-menu").addEventListener("click", () => {
-        window.location.href = "login.html";
-        localStorage.clear();
-    });
 
     function updateButtonState() {
         if (usernameError.textContent === "" && usernameInput.value.trim() !== "") {
